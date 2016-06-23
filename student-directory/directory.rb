26ef25db6103c3640272fadb30358e2ca1e97f55
Tim <tchung.nz@gmail.com>
2016-06-23 09:57:51 +0000

    Added hashes

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,22 +1,32 @@
 #First we print the list of students
-students = ["Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched",
-"Alex DeLarge", "The Wicked Witch of the West", "Terminator", "Freddy Krueger",
-"The Joker", "Joffrey Baratheon", "Norman Bates"]
+students = [
+    {name: "Dr. Hannibal Lecter", cohort: :november},
+    {name: "Darth Vader", cohort: :november},
+    {name: "Nurse Ratched", cohort: :november},
+    {name: "Alex DeLarge", cohort: :november},
+    {name: "The Wicked Witch of the West", cohort: :november},
+    {name: "Terminator", cohort: :november},
+    {name: "Freddy Krueger", cohort: :november},
+    {name: "The Joker", cohort: :november},
+    {name: "Joffrey Baratheon", cohort: :november},
+    {name: "Norman Bates", cohort: :november}
+    ]
 
 def print_header
 puts "The students of Villains Academy"
 puts "-------------"
 end
 
-def print_students(names)
-names.each do |student|
-    puts student
+def print_students(students)
+students.each do |student|
+    puts "#{student[:name]} #{student[:cohort]} cohort"
+end
 end
 
-def print_footer
-print "Overall, we have #{names.count} great students"
+def print_footer(students)
+print "Overall, we have #{students.count} great students"
 end
 
 print_header
 print_students(students)
-print_footer
+print_footer(students)
