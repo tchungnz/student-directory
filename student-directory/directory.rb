76b36ffef17418e49b69e5547f25bbf8c69b7164
Tim <tchung.nz@gmail.com>
2016-06-23 09:18:07 +0000

    Updated diretory

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,3 +1,4 @@
+#First we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#Finally, we print the number of students
+print "Overall, we have "
+print "11"
+puts " great students"
