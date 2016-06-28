209caf95cfffb98676ff164e37349ac16b31727f
Tim <tchung.nz@gmail.com>
2016-06-28 11:59:01 +0000

    Saving the data to a file

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,3 +1,41 @@
+@students =[]
+
+def interactive_menu
+    loop do
+        print_menu    
+        process(gets.chomp)
+    end
+end
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save the list to students.csv"
+    puts "9. Exit"
+end
+
+def show_student
+    print_header
+    cohort
+    print_students
+    print_footer
+end
+
+def process(selection)
+    case selection
+    when "1"
+    students_input
+    when "2"
+    show_student
+    when "3"
+    save_students
+    when "9"
+    exit
+    else
+    puts "I don't understand what you meant, try again!"
+    end
+end
+    
 def print_header
 puts "The students of Villains Academy"
 puts "-------------"
@@ -17,35 +55,32 @@ def cohort_input
 end
 
 def students_input
-students = []
 name_input
 cohort_input
     while !@name.empty? do
-        students << {name: @name, cohort: @cohort.to_sym}
-        if students.length == 1
-            puts "Now we have #{students.count} student"
-        else puts "Now we have #{students.count} students"
+        @students << {name: @name, cohort: @cohort.to_sym}
+        if @students.length == 1
+            puts "Now we have #{@students.count} student"
+        else puts "Now we have #{@students.count} students"
         end
         name_input
         cohort_input
     end
-   students
 end
 
-def cohort(students)
+def cohort
     @cohort_list = []
-    students.each do |student|
+    @students.each do |student|
         if !@cohort_list.include? student[:cohort]
             @cohort_list << student[:cohort]
         end
     end
-    puts @cohort_list
 end
 
-def print_students(students)
+def print_students
     @cohort_list.each do |month|
         puts "Cohort #{month}:"
-        students.each do |student|
+        @students.each do |student|
             if student[:cohort] == month
                 puts "#{student[:name]}"
             end
@@ -53,16 +88,22 @@ def print_students(students)
     end
 end
 
-def print_footer(students)
-    if students.size == 1
-    puts "Overall, we have #{students.count} great student"
+def print_footer
+    if @students.size == 1
+    puts "Overall, we have #{@students.count} great student"
     else
-        puts "Overall, we have #{students.count} great students"
+        puts "Overall, we have #{@students.count} great students"
     end
 end
 
-students = students_input
-print_header
-cohort(students)
-print_students(students)
-print_footer(students)
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
+end
+
+interactive_menu
